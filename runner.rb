2ddf68a7a5194c744da04f501094093dd70a62b7
Sarah Caplan <caplan.sarah@gmail.com>
2017-09-18 16:41:01 -0700

    Pass all but last test for calculate_product()

--- runner.rb
+++ runner.rb
@@ -10,9 +10,19 @@ p define_name() == "Caplan"
 
 # # EXERCISE 2
 
-# <Your code here>
+def calculate_product(numbers)
+  product = 1
+  numbers.each do |i|
+    product = product * i
+  end
+  product
+end
 
 p calculate_product([2,8,3]) == 48
+puts calculate_product([1,2,3]) == 6 # this test puts 'true' if calculate_product([1,2,3]) returns the correct value of 6
+puts calculate_product([0,-1,-10]) == 0
+puts calculate_product([1,-1,-10]) == 10
+puts calculate_product([]) == nil
 
 
 # # EXERCISE 3
