688692a1754b492256513d1314c5594566126895
Olivia Morgan <morgan.olivia@gmail.com>
2017-09-18 16:54:58 -0700

    Finish excercise 3

--- runner.rb
+++ runner.rb
@@ -13,28 +13,25 @@ def calculate_product(array)
   product
 end
 
-  #DRIVER CODE
-  p calculate_product([1, 2, 3])
-  p calculate_product([0, -1, 5])
-  p calculate_product([-1, -2, -3])
-
-
-
-
-
+#DRIVER CODE
+puts calculate_product([1, 2, 3]) == 6
+puts calculate_product([0, -1, 5]) == 0
+puts calculate_product([-1, -2, -3]) == 6 #only one that should return false
 
 # EXERCISE 3
 
 =begin
-  <Your answer here>
+  The method format_name is taking a name hash and then returning the values for the keys known as last and first.
+  It is returning them as "last name, first name"
+
+  The method display_name is also taking name hash and then PRINTING the format from format_name.
 =end
-
-
 
 
 # EXERCISE 4
 
-# <Your code here>
+def string_missing(str)
+numbers_array = str.split(",")
 
 
 
