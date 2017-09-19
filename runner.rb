03cf4f69d0329580685de3ec25492da2ddef00c7
Anica Lymus <email": null>
2017-09-18 18:03:33 -0700

    Add comment on what each methods fo and the values they return

--- runner.rb
+++ runner.rb
@@ -22,18 +22,20 @@ end
 # EXERCISE 3
 
 =begin
-Defining the format name and taking name as an argument and returning the last
+Defining the format_name and taking name as an argument and returning the last
 and first name of what is entered.
 
-Defining display_name and taking name as an argument and
+Defining display_name and taking name as an argument and displaying the name that
+was defined in the format name.
 =end
 
 
-
-
 # EXERCISE 4
+def find_missing_number(array)
+  sum = 0
 
-# <Your code here>
+
+end
 
 
 
@@ -46,6 +48,11 @@ puts calculate_product([1,2,3]) == 6 # this test puts 'true' if calculate_produc
 puts calculate_product([0,-1,-10]) == 0
 puts calculate_product([1,-1,-10]) == 10
 puts calculate_product([]) == nil
+
+
+puts find_missing_number(string_missing_7) == 7
+puts find_missing_number(string_missing_4567) == 4567
+puts find_missing_number(string_missing_9999) == 9999
 
 
 
