8f803d2cd79a073ce5df4c4294637f90835b1d68
Anica Lymus <email": null>
2017-09-18 17:34:47 -0700

    Add 2 local variables

--- runner.rb
+++ runner.rb
@@ -1,13 +1,20 @@
 # EXERCISE 1
-
-<Your code here>
+first_name = 'Anica'
+last_name = 'Lymus'
 
 
 
 
 # EXERCISE 2
-
-<Your code here>
+def calculate_product(array)
+  running_total = 1
+  array.each do |integer|
+    # p "tote: #{running_total}"
+    # p "int: #{integer}"
+    running_total *= integer
+  end
+  running_total
+end
 
 
 
@@ -15,7 +22,10 @@
 # EXERCISE 3
 
 =begin
-  <Your answer here>
+Defining the format name and taking name as an argument and returning the last
+and first name of what is entered.
+
+Defining display_name and taking name as an argument and
 =end
 
 
@@ -23,15 +33,19 @@
 
 # EXERCISE 4
 
-<Your code here>
+# <Your code here>
 
 
 
 
 # EXERCISE 5
 
-<Your code here>
+# <Your code here>
 
+puts calculate_product([1,2,3]) == 6 # this test puts 'true' if calculate_product([1,2,3]) returns the correct value of 6
+puts calculate_product([0,-1,-10]) == 0
+puts calculate_product([1,-1,-10]) == 10
+puts calculate_product([]) == nil
 
 
 
