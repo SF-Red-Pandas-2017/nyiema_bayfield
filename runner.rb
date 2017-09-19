9627e9504716ae4e138552e49ede3fe77f90f2d0
Sarah Caplan <caplan.sarah@gmail.com>
2017-09-18 17:12:40 -0700

    Add toggle_heater and toggle_ac methods

--- runner.rb
+++ runner.rb
@@ -1,21 +1,25 @@
 # EXERCISE 1
 
-def define_name()
-  first_name = "Sarah"
-  last_name = "Caplan"
-end
+first_name = "Sarah"
+last_name = "Caplan"
 
-p define_name() == "Caplan"
+p first_name == "Sarah"
+p last_name == "Caplan"
+
 
 
 # # EXERCISE 2
 
 def calculate_product(numbers)
-  product = 1
-  numbers.each do |i|
-    product = product * i
+  if numbers == []
+    return nil
+  else
+    product = 1
+    numbers.each do |i|
+      product = product * i
+    end
+    product
   end
-  product
 end
 
 p calculate_product([2,8,3]) == 48
@@ -27,8 +31,13 @@ puts calculate_product([]) == nil
 
 # # EXERCISE 3
 
+# def format_name(name)
+#   return "#{name[:last]}, #{name[:first]}"
+# end
 
-#   <Your answer here>
+# def display_name(name)
+#   puts format_name(name)
+# end
 
 
 
@@ -43,8 +52,38 @@ puts calculate_product([]) == nil
 
 # # EXERCISE 5
 
-# <Your code here>
+class House
 
+  def initialize(current_temp, minimum_temp, maximum_temp)
+    @current_temp = current_temp
+    @minimum_temp = minimum_temp
+    @maximum_temp = maximum_temp
+    @heater_on = false
+    @ac_on = false
+  end
+
+  def toggle_heater()
+    if heater_on = false && ac_on = true
+      return heater_on = false
+    elsif heater_on = true && ac_on = false
+      return heater_on = false
+    else heater_on = false && ac_on = false
+      return heater_on = true
+    end
+  end
+
+  def toggle_ac()
+    if ac_on = false && heater_on = true
+      return ac_on = false
+    elsif ac_on = true && heater_on = false
+      return ac_on = false
+    else ac_on = false && heater_on = false
+      return ac_on = true
+    end
+  end
+
+end
+# The House initialize method throws an ArgumentError if more or less than three arguments are passed.
 
 
 
