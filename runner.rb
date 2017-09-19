f05df64b84da3c607a9c5cdc0eeb55084fdb925c
Sarah Caplan <caplan.sarah@gmail.com>
2017-09-18 17:46:48 -0700

    Add plain English for ex 3

--- runner.rb
+++ runner.rb
@@ -30,14 +30,26 @@ puts calculate_product([]) == nil
 
 
 # # EXERCISE 3
+=begin
+First we define a method called format_name that takes one parameter that we'll call "name" here. Looking ahead at the block of code this method will execute we see the
+syntax name[:last] which tells us that the parameter's type is a hash. We know this because of the value-bracket-symbol-closed-bracket (eg [:last]) notation use in the block.
+The block of this method says the method should return a string and within that string we should return the values of the hash at the particular keys given.
+=end
+def format_name(name)
+  return "#{name[:last]}, #{name[:first]}"
+end
 
-# def format_name(name)
-#   return "#{name[:last]}, #{name[:first]}"
-# end
+=begin
+First we define a method called display_name that takes one parameter that we'll call "name" here. In the block of this method we are calling the method #format_name
+which is the method we defined above. The return value for #format_name is "#{name[:last]}, #{name[:first]}". Because there is a puts in front of this method in the block, we will see the returned value which is nil.
+=end
+def display_name(name)
+  puts format_name(name)
+end
 
-# def display_name(name)
-#   puts format_name(name)
-# end
+
+p format_name({first:"Sarah", last:"Caplan"}) == "Caplan, Sarah"
+p display_name({first:"Sarah", last:"Caplan"}) == nil
 
 
 
@@ -52,57 +64,57 @@ puts calculate_product([]) == nil
 
 # # EXERCISE 5
 
-class House
+# class House
 
-  def initialize(current_temp, minimum_temp, maximum_temp)
-    @current_temp = current_temp
-    @minimum_temp = minimum_temp
-    @maximum_temp = maximum_temp
-    @heater_on = false
-    @ac_on = false
-  end
+#   def initialize(current_temp, minimum_temp, maximum_temp)
+#     @current_temp = current_temp
+#     @minimum_temp = minimum_temp
+#     @maximum_temp = maximum_temp
+#     @heater_on = false
+#     @ac_on = false
+#   end
 
-  def toggle_heater
-    if @heater_on = false && @ac_on = true
-      return @heater_on = false
-    elsif @heater_on = true && @ac_on = false
-      return @heater_on = false
-    else @heater_on = false && @ac_on = false
-      return @heater_on = true
-    end
-  end
+#   def toggle_heater
+#     if @heater_on = false && @ac_on = true
+#       return @heater_on = false
+#     elsif @heater_on = true && @ac_on = false
+#       return @heater_on = false
+#     else @heater_on = false && @ac_on = false
+#       return @heater_on = true
+#     end
+#   end
 
-  def toggle_ac
-    if @ac_on = false && @heater_on = true
-      return @ac_on = false
-    elsif @ac_on = true && @heater_on = false
-      return @ac_on = false
-    else @ac_on = false && @heater_on = false
-      return @ac_on = true
-    end
-  end
+#   def toggle_ac
+#     if @ac_on = false && @heater_on = true
+#       return @ac_on = false
+#     elsif @ac_on = true && @heater_on = false
+#       return @ac_on = false
+#     else @ac_on = false && @heater_on = false
+#       return @ac_on = true
+#     end
+#   end
 
-  def update_temp!
-    if @heater_on = true
-      @current_temp += 1
-    else @ac_on = true
-      @current_temp += 2
-    end
-  end
+#   def update_temp!
+#     if @heater_on = true
+#       @current_temp += 1
+#     else @ac_on = true
+#       @current_temp += 2
+#     end
+#   end
 
-  def thermostat
-    if @current_temp > @maximum_temp
-      @ac_on = true
-      @heater_on = false
-    elsif @current_temp < @minimum_temp
-      @heater_on = true
-      @ac_on = false
-    end
-  end
-end
-# The House initialize method throws an ArgumentError if more or less than three arguments are passed.
-## Driver code
-my_house = House.new(75, 60, 80)
+#   def thermostat
+#     if @current_temp > @maximum_temp
+#       @ac_on = true
+#       @heater_on = false
+#     elsif @current_temp < @minimum_temp
+#       @heater_on = true
+#       @ac_on = false
+#     end
+#   end
+# end
+# # The House initialize method throws an ArgumentError if more or less than three arguments are passed.
+# ## Driver code
+# my_house = House.new(75, 60, 80)
 
 
 
